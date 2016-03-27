bf461d33e6ff8c6695dee15442cdaf1ad6178670
resivalex <resivalex@gmail.com>
2016-03-27 16:46:21 +0300

    Unicorn restart settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,3 +53,20 @@ namespace :deploy do
   end
 
 end
+
+namespace :deploy do
+  desc "Start the Unicorn process when it isn't already running."
+  task :start do
+    run "cd #{current_path} && #{current_path}/bin/unicorn -Dc #{current_path}/config/unicorn.rb -E #{rails_env}"
+  end
+
+  desc "Initiate a rolling restart by telling Unicorn to start the new application code and kill the old process when done."
+  task :restart do
+    run "kill -USR2 $(cat #{shared_path}/unicorn.pid)"
+  end
+
+  desc "Stop the application by killing the Unicorn process"
+  task :stop do
+    run "kill $(cat #{shared_path}/unicorn.pid)"
+  end
+end
